90115e3ba464ff483949700c9e60a6987cf17afb
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-03-22 16:40:02 +0100

    add mixed chart

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
-  <div id="app">
-    <h1>Le coronavirus en france</h1>
-    <p>blablabla</p>
-    <h2>Source des données</h2>
-    <p>Santé Publique France blablabla</p>
-    <h2>Évolution du nombre de cas en France</h2>
-    <p>blablablabla</p>
-    <line-chart :chartData='formatFranceLineChart()' :styles="chartHeight"></line-chart>
-    <h2>Évolution du nombre de cas dans chaque région</h2>
-    <p>blablablabla</p>
+  <div id='app'>
+    <div class='lastUpdate'>mis à jour le {{ lastUpdate }}</div>
+    <div class='content'>
+      <h1>Le coronavirus en france</h1>
+      <p>Quelques visualisations sur l'évolution de l'épidémie du coronavirus en France.</p>
+      <h2>Source des données</h2>
+      <p>
+        Les données à l'origine des graphes ci-dessous proviennent de <a href='https://www.data.gouv.fr/fr/datasets/cas-confirmes-dinfection-au-covid-19-par-region/'>data.gouv.fr</a>,
+        la plateforme d'open data du gouvernement français. Elles sont mises à disposition par
+        <a href="https://www.data.gouv.fr/fr/users/bastien-guerry/">Bastien Guerry</a> (ingénieur chez Etalab) qui agrège et
+        met en forme les informations données par Santé Publique France lors de ses <a href='https://www.santepubliquefrance.fr/maladies-et-traumatismes/maladies-et-infections-respiratoires/infection-a-coronavirus/articles/infection-au-nouveau-coronavirus-sars-cov-2-covid-19-france-et-monde'>points quotidiens</a>
+        sur l'épidémie du coronavirus.
+      </p>
+      <h2>Validité des données</h2>
+      <p>
+        Les données de Santé Publique France sur le nombre de cas <b>confirmés</b> d'infection au coronavirus par région
+        est très certainement loin du nombre de cas <b>réels</b> d'infection. Les consignes du gouvernement sont claires&nbsp;:
+      </p>
+      <p><i>Je me fais tester uniquement si je suis une personne fragile ou à risque, si je présente des signes de gravité [...]</i></p>
+      <p>
+        Une personne infectée mais initialement en bonne santé ne sera donc probablement pas répertoriée dans ces données.
+        De même, les personnes asymptomatiques (infectées mais sans aucun symptôme clinique) ne seront pas non plus répertoriées.
+      </p>
+      <p>
+        La valeur absolue de ces données n'a donc pas un grand intérêt. C'est plutôt la tendance qui est ici intéressante.
+      </p>
+      <h2>Évolution du nombre de cas confirmés en France</h2>
+      <p>En bleu : le nombre de cas confirmés d'infection au coronavirus sur le territoire français.</p>
+      <p>En rose : l'augmentation du nombre de cas entre le jour J et le jour J-1.</p>
+    </div>
+
+    <!-- first chart -->
+    <mixed-chart :chartData='formatFranceLineChart()' :styles="chartHeight"></mixed-chart>
+  
+    <div class='content'>
+      <h2>Évolution du nombre de cas confirmés dans chaque région</h2>
+      <p>Pour y voir plus clair, vous pouvez cliquer sur la légende correspondant à la courbe que vous souhaitez retirer.</p>
+    </div>
+
+    <!-- second chart -->
     <line-chart :chartData='formatRegionLineChart()' :styles="chartHeight"></line-chart>
+
   </div>
 </template>
 
@@ -19,11 +50,13 @@ import covid19 from './data/covid19.json';
 
 // components
 import LineChart from './components/LineChart.vue';
+import MixedChart from './components/MixedChart.vue';
 
 export default {
   name: 'App',
   components: {
     LineChart,
+    MixedChart,
   },
   mounted() {
     this.covid19 = covid19;
@@ -49,17 +82,34 @@ export default {
   methods: {
     formatFranceLineChart() {
       const labels = covid19.map(elem => elem.Date);
+      const test = 
+      console.log(test);
       return {
         labels,
-        datasets: [{
-          label: `France${String.fromCharCode(160)}`,
-          pointBackgroundColor: this.colors[0],
-          borderColor: this.colors[0],
-          pointBorderColor: this.colors[0],
-          fill: false,
-          lineTension: 0.1,
-          data: covid19.map(elem => elem.France),
-        }],
+        datasets: [
+          {
+            label: `France${String.fromCharCode(160)}`,
+            type: 'line',
+            pointBackgroundColor: this.colors[0],
+            borderColor: this.colors[0],
+            pointBorderColor: this.colors[0],
+            fill: false,
+            lineTension: 0.1,
+            yAxisID: 'left-axis',
+            data: covid19.map(elem => elem.France),
+          },
+          {
+            label: `Évolution J-1 (%)${String.fromCharCode(160)}`,
+            type: 'bar',
+            backgroundColor: 'pink',
+            borderColor: 'pink',
+            yAxisID: 'right-axis',
+            borderWidth: 3,
+            data: covid19.map((elem, index) => {
+              return index === 0 ? 0 : Math.round((elem.France / covid19[index - 1].France - 1) * 100) || 0;
+            }),
+          },
+        ],
       };
     },
     formatRegionLineChart() {
@@ -83,6 +133,9 @@ export default {
     },
   },
   computed: {
+    lastUpdate() {
+      return covid19[covid19.length - 1].Date;
+    },
     chartHeight() {
       return {
         height: '500px',
@@ -104,10 +157,21 @@ export default {
   margin-top: 40px;
 }
 
-/* If the screen size is 601px wide or more, set max-width to 700px  */
+.lastUpdate {
+  position:absolute;
+  top: 10px;
+  right: 10px;
+}
+
+/* If the screen size is 601px wide or more, set max-width to 80%  */
 @media screen and (min-width: 601px) {
-  #app {
-    max-width: 1000px;
+  .content {
+    max-width: 750px;
+    margin: auto;
+  }
+  canvas {
+    max-width: 1200px;
+    margin: auto;
   }
   p {
     font-size: 21px;
@@ -128,5 +192,9 @@ export default {
   a {
     font-size: 18px;
   }
+}
+
+p {
+  text-align: left;
 }
 </style>
---
+++ src/components/MixedChart.vue
@@ -0,0 +1,76 @@
+<script>
+import { Bar } from 'vue-chartjs'
+import 'chartjs-plugin-annotation';
+
+export default {
+  name: 'MixedChart',
+  extends: Bar,
+  props: {
+    chartData:  {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+  data() {
+    return {
+      options: {
+        scales: {
+          yAxes: [
+            {
+              type: 'linear',
+              id:'left-axis',
+              display: true,
+              position: 'left',
+              ticks: { beginAtZero: true },
+              gridLines: { display: true }
+            },
+            {
+              type: 'linear',
+              id: 'right-axis',
+              display: true,
+              position: 'right',
+              stacked: false,
+              scaleLabel: { display: true, labelString: '%' },
+              gridLines: { drawOnChartArea: false }
+            },
+          ],
+          xAxes: [ {
+            gridLines: {
+              display: true,
+            },
+          }]
+        },
+        legend: {
+          display: true
+        },
+        tooltips: {
+          backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        annotation: {
+          annotations: [
+            {
+              type: 'line',
+              mode: 'vertical',
+              scaleID: 'x-axis-0',
+              value: '17/03/2020',
+              borderColor: 'red',
+              borderWidth: 2,
+              label: {
+                enabled: true,
+                position: 'top',
+                backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                content: 'DÉBUT CONFINEMENT',
+              },
+            },
+          ],
+        },
+      },
+    };
+  },
+};
+</script>
