5941648a3829f02b451009d8039727518caca01c
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-04-12 15:44:54 +0200

    fix again 404 bad redirection

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,15 @@
 <template>
-  <v-container class="fill-height" fluid>
-    <h1 class="display-2 text-center">La page que vous cherchez n'existe pas 😢</h1>
+  <v-container class="fill-height">
+    <v-row class="mt-5">
+      <v-col cols="12">
+        <h1 class="display-2 text-center">La page que vous cherchez n'existe pas 😢</h1>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-
+  name: 'NotFound',
 }
 </script>
